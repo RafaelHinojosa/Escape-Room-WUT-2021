5fbaf7cbb6b76e248a985d5c7fc01740ede9de39
RafaelHinojosa <rafael.zeo@gmail.com>
2021-04-26 23:29:47 -0500

    Login Finalizado

--- JS/index.js
+++ JS/index.js
@@ -4,12 +4,14 @@ boton.addEventListener('click', function() {
     let user = document.getElementById("user").value;
     let password = document.getElementById("password").value;
     
+    let botonSiguiente = document.getElementById("boton-siguiente");
     let veredicto = document.getElementById("veredicto");
 
     if(user === "3251" && password === "hola") {
-        veredicto.innerHTML = '<a href="reto1.html"/><button>Siguiente</button></a>';
+        botonSiguiente.innerHTML = '<a href="reto1.html"/><button>Siguiente</button></a>';
+        veredicto.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;Adelante agente...";
     }
     else {
-        veredicto.innerHTML = "Incorrecto";
+        veredicto.innerHTML = "Identidad Desconocida";
     }    
 });
