9f92c0dc8413693079915bc78df98da247f4cd32
RafaelHinojosa <rafael.zeo@gmail.com>
2021-05-04 21:35:29 -0500

    Rutas y controlador de login hechos

--- controller/controller.js
+++ controller/controller.js
@@ -1,1 +1,22 @@
 // Aqui iran todos los gets, posts
+
+const bcrypt = require('bcryptjs');
+
+exports.getLogin = (request, response, next) => {
+    response.render('index');
+};
+
+exports.postLogin = (request, response, next) => {
+    let user = request.body.username; 
+    let password = request.body.password;
+
+    if(user === "3251" && password === "hola") {
+        console.log('Siu');
+        request.session.acceso = 1;
+        console.log(request.session.acceso);
+    }
+    else {
+        console.log('Intruso detectado');
+        response.redirect('/agentes/login');
+    }
+}
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+// const isAuth = require('../util/isAuth');
+const path = require('path');
 
 // Aqui van las rutas...
 const rutas = require('../controller/controller');
 
 // router.get, para todas
+
+// get login
+// post login
+// tal vez incluir en el script la validaci[on de contrasenas pero no me late] 
+// get reto1
+// post reto1
+// get reto2
+// post reto2
+// get/post ... reto final
+// get felicidades....
+
+router.get('/login', rutas.getLogin);
+
+router.post('/login', rutas.postLogin);
+
+router.use(express.static(path.join(__dirname, '..', 'public')));
 
 module.exports = router;
